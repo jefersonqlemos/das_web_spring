381f62f4e670a1339bcb3dd8c28c759aa141ed1c
jeferson lemos <jefersonquagliottolemos@hotmail.com>
2022-08-20 11:31:26 -0300

    first commit

---
+++ src/main/java/com/das_web/das_web_spring/DasWebSpringApplication.java
@@ -0,0 +1,13 @@
+package com.das_web.das_web_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasWebSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasWebSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/das_web/das_web_spring/DasWebSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.das_web.das_web_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DasWebSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
